d5b7aff79a2994bc37da8babd1237d7d2095c580
ray <ray81415@gmail.com>
2023-08-11 17:29:28 +0800

    feat: add the random

---
+++ pkg/random/random.go
@@ -0,0 +1,24 @@
+package random
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"strings"
+)
+
+func RandomString(n int) string {
+	replacer := strings.NewReplacer("/", "", "+", "")
+	str := ""
+
+	for len(str) < n {
+		b := make([]byte, n-len(str))
+		_, err := rand.Read(b)
+		if err != nil {
+			panic(err)
+		}
+		str = str + base64.RawStdEncoding.EncodeToString(b)
+		str = replacer.Replace(str)
+	}
+
+	return str[:n]
+}
