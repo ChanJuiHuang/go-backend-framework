22613a7cb54f9b7a668cc49aa7b95156b8c69c45
ray <ray81415@gmail.com>
2023-09-12 16:47:36 +0800

    feat: add the user register test

---
+++ internal/http/controller/user/user_register_test.go
@@ -0,0 +1,88 @@
+package user_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/ChanJuiHuang/go-backend-framework/internal/http/controller/user"
+	"github.com/ChanJuiHuang/go-backend-framework/internal/http/response"
+	"github.com/ChanJuiHuang/go-backend-framework/internal/test"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+)
+
+type UserRegisterTestSuite struct {
+	suite.Suite
+}
+
+func (suite *UserRegisterTestSuite) SetupSuite() {
+	test.Migration.Run()
+}
+
+func (suite *UserRegisterTestSuite) TestRegister() {
+	userRegisterRequest := user.UserRegisterRequest{
+		Name:     "bob",
+		Email:    "bob@test.com",
+		Password: "abcABC123",
+	}
+	reqBody, err := json.Marshal(userRegisterRequest)
+	if err != nil {
+		panic(err)
+	}
+
+	req := httptest.NewRequest("POST", "/api/user/register", bytes.NewReader(reqBody))
+	test.AddCsrfToken(req)
+	resp := httptest.NewRecorder()
+	test.HttpHandler.ServeHTTP(resp, req)
+
+	respBody := &user.UserRegisterResponse{}
+	if err := json.Unmarshal(resp.Body.Bytes(), &respBody); err != nil {
+		panic(err)
+	}
+
+	assert.Equal(suite.T(), http.StatusOK, resp.Code)
+	assert.NotEmpty(suite.T(), respBody.AccessToken)
+	assert.NotEmpty(suite.T(), respBody.RefreshToken)
+}
+
+func (suite *UserRegisterTestSuite) TestCsrfMismatch() {
+	req := httptest.NewRequest("POST", "/api/user/register", bytes.NewReader([]byte{}))
+	resp := httptest.NewRecorder()
+	test.HttpHandler.ServeHTTP(resp, req)
+
+	respBody := &response.ErrorResponse{}
+	if err := json.Unmarshal(resp.Body.Bytes(), &respBody); err != nil {
+		panic(err)
+	}
+
+	assert.Equal(suite.T(), http.StatusForbidden, resp.Code)
+	assert.Equal(suite.T(), response.Forbidden, respBody.Message)
+	assert.Equal(suite.T(), response.MessageToCode[response.Forbidden], respBody.Code)
+}
+
+func (suite *UserRegisterTestSuite) TestRequestValidationFailed() {
+	req := httptest.NewRequest("POST", "/api/user/register", bytes.NewReader([]byte{}))
+	test.AddCsrfToken(req)
+	resp := httptest.NewRecorder()
+	test.HttpHandler.ServeHTTP(resp, req)
+
+	respBody := &response.ErrorResponse{}
+	if err := json.Unmarshal(resp.Body.Bytes(), &respBody); err != nil {
+		panic(err)
+	}
+
+	assert.Equal(suite.T(), http.StatusBadRequest, resp.Code)
+	assert.Equal(suite.T(), response.RequestValidationFailed, respBody.Message)
+	assert.Equal(suite.T(), response.MessageToCode[response.RequestValidationFailed], respBody.Code)
+}
+
+func (suite *UserRegisterTestSuite) TearDownSuite() {
+	test.Migration.Reset()
+}
+
+func TestUserRegisterTestSuite(t *testing.T) {
+	suite.Run(t, new(UserRegisterTestSuite))
+}
