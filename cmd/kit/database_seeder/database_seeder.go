8fd77b541f515a9f2a4bd15de85242622f447961
ray <ray81415@gmail.com>
2023-10-01 10:55:34 +0800

    chore: use command template in the database seeder

--- cmd/kit/database_seeder/database_seeder.go
+++ cmd/kit/database_seeder/database_seeder.go
@@ -1,40 +1,21 @@
 package main
 
 import (
-	"os"
-	"path"
-	"strings"
-
 	_ "github.com/joho/godotenv/autoload"
 
 	"github.com/ChanJuiHuang/go-backend-framework/internal/migration/seeder"
 	"github.com/ChanJuiHuang/go-backend-framework/internal/pkg/provider"
-	"github.com/ChanJuiHuang/go-backend-framework/pkg/config"
-	"github.com/ChanJuiHuang/go-backend-framework/pkg/database"
-	"github.com/spf13/viper"
 )
 
 func init() {
-	wd, err := os.Getwd()
-	if err != nil {
-		panic(err)
-	}
-	byteYaml, err := os.ReadFile(path.Join(wd, "config.yml"))
-	if err != nil {
-		panic(err)
-	}
-	stringYaml := os.ExpandEnv(string(byteYaml))
+	globalConfig := newGlobalConfig()
+	registerGlobalConfig(globalConfig)
+	setEnv(*globalConfig)
+	registerConfig(*globalConfig)
 
-	v := viper.New()
-	v.SetConfigType("yaml")
-	v.ReadConfig(strings.NewReader(stringYaml))
-
-	config.Registry.SetViper(v)
-	config.Registry.Register(map[string]any{
-		"database": &database.Config{},
-	})
+	registerProvider()
 }
 
 func main() {
-	seeder.Run(provider.ProvideDB())
+	seeder.Run(provider.Registry.DB())
 }
