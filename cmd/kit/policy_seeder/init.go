a7305b28fa7b8a1107c1efd2973f51acd71fca8b
ray <ray81415@gmail.com>
2023-10-01 11:27:25 +0800

    chore: use command template in the policy seeder

---
+++ cmd/kit/policy_seeder/init.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"os"
+	"path"
+	"strings"
+
+	"github.com/ChanJuiHuang/go-backend-framework/internal/global"
+	"github.com/ChanJuiHuang/go-backend-framework/internal/pkg/provider"
+	"github.com/ChanJuiHuang/go-backend-framework/pkg/authentication"
+	"github.com/ChanJuiHuang/go-backend-framework/pkg/config"
+	"github.com/ChanJuiHuang/go-backend-framework/pkg/database"
+	"github.com/ChanJuiHuang/go-backend-framework/pkg/logger"
+	"github.com/spf13/viper"
+)
+
+func newGlobalConfig() *global.Config {
+	wd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	globalConfig := &global.Config{
+		RootDir:  wd,
+		Timezone: "UTC",
+		Debug:    false,
+		Testing:  false,
+	}
+
+	return globalConfig
+}
+
+func registerGlobalConfig(globalConfig *global.Config) {
+	config.Registry.Set("global", globalConfig)
+}
+
+func setEnv(globalConfig global.Config) {
+	err := os.Setenv("TZ", globalConfig.Timezone)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func registerConfig(globalConfig global.Config) {
+	byteYaml, err := os.ReadFile(path.Join(globalConfig.RootDir, "config.yml"))
+	if err != nil {
+		panic(err)
+	}
+	stringYaml := os.ExpandEnv(string(byteYaml))
+
+	v := viper.New()
+	v.SetConfigType("yaml")
+	v.ReadConfig(strings.NewReader(stringYaml))
+
+	config.Registry.SetViper(v)
+	config.Registry.Register(map[string]any{
+		"logger.console":               &logger.ConsoleConfig{},
+		"logger.file":                  &logger.FileConfig{},
+		"database":                     &database.Config{},
+		"authentication.authenticator": &authentication.Config{},
+	})
+}
+
+func registerProvider() {
+	logger, consoleLogger, fileLogger := provider.ProvideLogger()
+	db := provider.ProvideDB()
+
+	provider.Registry.Register(
+		logger,
+		consoleLogger,
+		fileLogger,
+		db,
+		nil,
+		provider.ProvideAuthenticator(),
+		provider.ProvideCasbin(db),
+	)
+}
--- cmd/kit/policy_seeder/policy_seeder.go
+++ cmd/kit/policy_seeder/policy_seeder.go
@@ -1,46 +1,26 @@
 package main
 
 import (
-	"log"
-	"os"
-	"path"
 	"strconv"
-	"strings"
 
 	_ "github.com/joho/godotenv/autoload"
 
 	"github.com/ChanJuiHuang/go-backend-framework/internal/pkg/provider"
 	"github.com/ChanJuiHuang/go-backend-framework/internal/pkg/user/model"
-	"github.com/ChanJuiHuang/go-backend-framework/pkg/config"
-	"github.com/ChanJuiHuang/go-backend-framework/pkg/database"
 	"github.com/casbin/casbin/v2"
 	gormadapter "github.com/casbin/gorm-adapter/v3"
-	"github.com/spf13/viper"
-	"gorm.io/gorm"
 )
 
 func init() {
-	wd, err := os.Getwd()
-	if err != nil {
-		panic(err)
-	}
-	byteYaml, err := os.ReadFile(path.Join(wd, "config.yml"))
-	if err != nil {
-		panic(err)
-	}
-	stringYaml := os.ExpandEnv(string(byteYaml))
+	globalConfig := newGlobalConfig()
+	registerGlobalConfig(globalConfig)
+	setEnv(*globalConfig)
+	registerConfig(*globalConfig)
 
-	v := viper.New()
-	v.SetConfigType("yaml")
-	v.ReadConfig(strings.NewReader(stringYaml))
-
-	config.Registry.SetViper(v)
-	config.Registry.Register(map[string]any{
-		"database": &database.Config{},
-	})
+	registerProvider()
 }
 
-func addPolicies(enforcer *casbin.SyncedCachedEnforcer) {
+func addPolicies() {
 	policies := [][]string{
 		{"admin", "/api/admin/policy", "POST"},
 		{"admin", "/api/admin/policy", "DELETE"},
@@ -52,20 +32,22 @@ func addPolicies(enforcer *casbin.SyncedCachedEnforcer) {
 		{"admin", "/api/admin/grouping-policy/:userId", "GET"},
 		{"admin", "/api/admin/grouping-policy", "DELETE"},
 	}
+	enforcer := provider.Registry.Casbin()
+	logger := provider.Registry.Logger()
 
 	err := enforcer.GetAdapter().(*gormadapter.Adapter).Transaction(enforcer, func(e casbin.IEnforcer) error {
 		result, err := e.RemovePolicies(policies)
 		if err != nil {
-			log.Println(err)
+			logger.Error(err.Error())
 			return err
 		}
 		if !result {
-			log.Println("policies is not in [casbin_rules] table PROBABLY")
+			logger.Warn("policies are not in [casbin_rules] table PROBABLY")
 		}
 
 		result, err = enforcer.AddPolicies(policies)
 		if err != nil {
-			log.Println(err)
+			logger.Error(err.Error())
 			return err
 		}
 		if !result {
@@ -80,11 +62,11 @@ func addPolicies(enforcer *casbin.SyncedCachedEnforcer) {
 	}
 }
 
-func addGroupingPolicies(db *gorm.DB, enforcer *casbin.SyncedCachedEnforcer) {
+func addGroupingPolicies() {
 	user := &model.User{}
-	tx := db.Where("email = ?", "admin@admin.com").
+	db := provider.Registry.DB().Where("email = ?", "admin@admin.com").
 		First(user)
-	if err := tx.Error; err != nil {
+	if err := db.Error; err != nil {
 		panic(err)
 	}
 
@@ -92,19 +74,22 @@ func addGroupingPolicies(db *gorm.DB, enforcer *casbin.SyncedCachedEnforcer) {
 		{strconv.Itoa(int(user.Id)), "admin"},
 	}
 
+	enforcer := provider.Registry.Casbin()
+	logger := provider.Registry.Logger()
+
 	err := enforcer.GetAdapter().(*gormadapter.Adapter).Transaction(enforcer, func(e casbin.IEnforcer) error {
-		result, err := enforcer.RemoveGroupingPolicies(groupingPolicies)
+		result, err := e.RemoveGroupingPolicies(groupingPolicies)
 		if err != nil {
-			log.Println(err)
+			logger.Error(err.Error())
 			return err
 		}
 		if !result {
-			log.Println("grouping policies is not in [casbin_rules] table PROBABLY")
+			logger.Warn("grouping policies are not in [casbin_rules] table PROBABLY")
 		}
 
-		result, err = enforcer.AddGroupingPolicies(groupingPolicies)
+		result, err = e.AddGroupingPolicies(groupingPolicies)
 		if err != nil {
-			log.Println(err)
+			logger.Error(err.Error())
 			return err
 		}
 		if !result {
@@ -120,10 +105,6 @@ func addGroupingPolicies(db *gorm.DB, enforcer *casbin.SyncedCachedEnforcer) {
 }
 
 func main() {
-	db := provider.ProvideDB()
-	enforcer := provider.ProvideCasbin(db)
-	log.SetFlags(log.LstdFlags | log.Llongfile)
-
-	addPolicies(enforcer)
-	addGroupingPolicies(db, enforcer)
+	addPolicies()
+	addGroupingPolicies()
 }
