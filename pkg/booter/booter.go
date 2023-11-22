1d2f9fd2ab6b25b03764f8d4368d781f010746b9
ray <ray81415@gmail.com>
2023-11-22 18:36:31 +0800

    feat: add the booter package

---
+++ pkg/booter/booter.go
@@ -0,0 +1,112 @@
+package booter
+
+import (
+	"flag"
+	"os"
+	"path"
+	"runtime"
+	"strings"
+
+	"github.com/ChanJuiHuang/go-backend-framework/pkg/booter/config"
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	RootDir        string
+	ConfigFileName string
+	Debug          bool
+	Testing        bool
+}
+
+func NewConfig(rootDir string, configFileName string, debug bool, testing bool) *Config {
+	return &Config{
+		RootDir:        rootDir,
+		ConfigFileName: configFileName,
+		Debug:          debug,
+		Testing:        testing,
+	}
+}
+
+func NewConfigWithCommand() *Config {
+	wd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	var rootDir string
+	var configFileName string
+	var debug bool
+	var testing bool
+	flag.StringVar(&rootDir, "rootDir", wd, "root directory which the executable file in")
+	flag.StringVar(&configFileName, "configFileName", "config.yml", "config file name")
+	flag.BoolVar(&debug, "debug", false, "debug mode")
+	flag.BoolVar(&testing, "testing", false, "does run in testing mode")
+	flag.Parse()
+
+	return NewConfig(rootDir, configFileName, debug, testing)
+}
+
+func NewDefaultConfig() *Config {
+	wd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	return NewConfig(wd, "config.yml", false, false)
+}
+
+func NewProductionConfig() *Config {
+	wd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	return NewConfig(wd, "config.production.yml", false, false)
+}
+
+func NewTestingConfig() *Config {
+	_, file, _, ok := runtime.Caller(0)
+	if !ok {
+		panic("runtime caller cannot get file information")
+	}
+	wd := path.Join(path.Dir(file), "../..")
+
+	return NewConfig(wd, "config.testing.yml", false, true)
+}
+
+type NewConfigFunc func() *Config
+type LoadEnvFunc func()
+
+type ConfigRegistrar interface {
+	Register()
+}
+type ServiceRegistrar interface {
+	Register()
+}
+
+func bootConfigRegistry(booterConfig *Config) {
+	config.Registry.Set("booter", booterConfig)
+	byteYaml, err := os.ReadFile(path.Join(booterConfig.RootDir, booterConfig.ConfigFileName))
+	if err != nil {
+		panic(err)
+	}
+	stringYaml := os.ExpandEnv(string(byteYaml))
+
+	v := viper.New()
+	v.SetConfigType("yaml")
+	v.ReadConfig(strings.NewReader(stringYaml))
+
+	config.Registry.SetViper(v)
+}
+
+func Boot(
+	loadEnvFunc LoadEnvFunc,
+	newConfigFunc NewConfigFunc,
+	configRegistrar ConfigRegistrar,
+	serviceRegistrar ServiceRegistrar,
+) {
+	loadEnvFunc()
+	bootConfigRegistry(newConfigFunc())
+	configRegistrar.Register()
+	serviceRegistrar.Register()
+}
---
+++ pkg/booter/service/registry.go
@@ -0,0 +1,46 @@
+package service
+
+import "reflect"
+
+type registry struct {
+	services map[string]any
+}
+
+var Registry *registry
+
+func init() {
+	Registry = &registry{
+		services: map[string]any{},
+	}
+}
+
+func NewRegistry() *registry {
+	return &registry{
+		services: map[string]any{},
+	}
+}
+
+func (r *registry) Set(key string, service any) {
+	if !(reflect.ValueOf(service).Kind() == reflect.Pointer) {
+		panic("config is not the pointer")
+	}
+	r.services[key] = service
+}
+
+func (r *registry) SetMany(services map[string]any) {
+	for key, service := range services {
+		r.Set(key, service)
+	}
+}
+
+func (r *registry) Get(key string) any {
+	v := reflect.ValueOf(r.services[key])
+
+	return v.Interface()
+}
+
+func (r *registry) Clone(key string) any {
+	v := reflect.ValueOf(r.services[key])
+
+	return v.Elem().Interface()
+}
