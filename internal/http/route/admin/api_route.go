c0f282f5e1d3ab71cb13cdbc5a436bdfb74ac957
ray <ray81415@gmail.com>
2023-12-28 12:40:45 +0800

    chore: update the admin router

--- internal/http/route/admin/api_route.go
+++ internal/http/route/admin/api_route.go
@@ -12,17 +12,16 @@ type Router struct {
 
 func NewRouter(router *gin.Engine) *Router {
 	return &Router{
-		router: router.Group("api/admin"),
+		router: router.Group(
+			"api/admin",
+			middleware.Authenticate(),
+			middleware.Authorize(),
+		),
 	}
 }
 
 func (r *Router) AttachRoutes() {
-	r.router.GET(
-		"user/:userId/grouping-policy",
-		middleware.Authenticate(),
-		middleware.Authorize(),
-		admin.GetUserGroupingPolicy,
-	)
+	r.router.GET("user/:userId/grouping-policy", admin.GetUserGroupingPolicy)
 	r.AttachPolicyRoutes()
 	r.AttachPolicySubjectRoutes()
 	r.AttachGroupingPolicyRoutes()
@@ -30,66 +29,21 @@ func (r *Router) AttachRoutes() {
 
 func (r *Router) AttachPolicyRoutes() {
 	policyRouter := r.router.Group("policy")
-	policyRouter.POST(
-		"",
-		middleware.Authenticate(),
-		middleware.Authorize(),
-		admin.CreatePolicy,
-	)
-	policyRouter.DELETE(
-		"",
-		middleware.Authenticate(),
-		middleware.Authorize(),
-		admin.DeletePolicy,
-	)
-	policyRouter.POST(
-		"reload",
-		middleware.Authenticate(),
-		middleware.Authorize(),
-		admin.ReloadPolicy,
-	)
+	policyRouter.POST("", admin.CreatePolicy)
+	policyRouter.DELETE("", admin.DeletePolicy)
+	policyRouter.POST("reload", admin.ReloadPolicy)
 }
 
 func (r *Router) AttachPolicySubjectRoutes() {
 	policySubjectRouter := r.router.Group("policy/subject")
-	policySubjectRouter.GET(
-		"",
-		middleware.Authenticate(),
-		middleware.Authorize(),
-		admin.SearchPolicySubject,
-	)
-	policySubjectRouter.GET(
-		":subject",
-		middleware.Authenticate(),
-		middleware.Authorize(),
-		admin.GetPolicySubject,
-	)
-	policySubjectRouter.GET(
-		":subject/user",
-		middleware.Authenticate(),
-		middleware.Authorize(),
-		admin.GetPolicySubjectUser,
-	)
-	policySubjectRouter.DELETE(
-		"",
-		middleware.Authenticate(),
-		middleware.Authorize(),
-		admin.DeletePolicySubject,
-	)
+	policySubjectRouter.GET("", admin.SearchPolicySubject)
+	policySubjectRouter.GET(":subject", admin.GetPolicySubject)
+	policySubjectRouter.GET(":subject/user", admin.GetPolicySubjectUser)
+	policySubjectRouter.DELETE("", admin.DeletePolicySubject)
 }
 
 func (r *Router) AttachGroupingPolicyRoutes() {
 	groupingPolicyRouter := r.router.Group("grouping-policy")
-	groupingPolicyRouter.POST(
-		"",
-		middleware.Authenticate(),
-		middleware.Authorize(),
-		admin.CreateGroupingPolicy,
-	)
-	groupingPolicyRouter.DELETE(
-		"",
-		middleware.Authenticate(),
-		middleware.Authorize(),
-		admin.DeleteGroupingPolicy,
-	)
+	groupingPolicyRouter.POST("", admin.CreateGroupingPolicy)
+	groupingPolicyRouter.DELETE("", admin.DeleteGroupingPolicy)
 }
